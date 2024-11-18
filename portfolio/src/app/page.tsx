b1cb36abaf8ac248736ed2a908eefba67725ddc1
Jaomar92 <65635396+Jaomar92@users.noreply.github.com>
2024-11-18 23:34:24 +0700

    reinitiated project

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-500 py-3">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold text-sky-500">Hello World</h1>
+      </div>
+    </div>
+  );
+}
